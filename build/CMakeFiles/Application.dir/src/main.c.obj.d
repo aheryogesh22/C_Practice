863d79cc59dc777101bf4898aeb7c98ceac1369e
Yogesh Aher <aheryogesh22@gmail.com>
2025-11-30 17:44:09 +0530

    Remove build folder from tracking

--- build/CMakeFiles/Application.dir/src/main.c.obj.d
+++
@@ -1,8 +0,0 @@
-CMakeFiles/Application.dir/src/main.c.obj: D:\C_Practise\src\main.c \
- c:\mingw\include\stdio.h c:\mingw\include\_mingw.h \
- c:\mingw\include\msvcrtver.h c:\mingw\include\w32api.h \
- c:\mingw\include\sdkddkver.h \
- c:\mingw\lib\gcc\mingw32\6.3.0\include\stddef.h \
- c:\mingw\include\sys/types.h \
- c:\mingw\lib\gcc\mingw32\6.3.0\include\stdarg.h \
- D:/C_Practise/include/RingBuffer.h
